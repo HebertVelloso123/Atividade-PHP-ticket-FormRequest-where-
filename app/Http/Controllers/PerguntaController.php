dddd04dbf0894d620872dfde2ad11194d8543767
HebertVelloso123 <22400397@aluno.cotemig.com.br>
2026-09-24 11:14:20 -0300

    Add files via upload

---
+++ app/Http/Controllers/PerguntaController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Controllers\PerguntaController;
+
+Route::post('/eventos/{evento}/perguntas', [PerguntaController::class, 'store'])
+    ->middleware('auth')
+    ->name('perguntas.store');
